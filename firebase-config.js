0bf7a13ad304ae52e385d13900e2f457edb4a552
zoomcom4 <zoomcom31000@gmail.com>
2026-09-19 11:12:19 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,41 +1,38 @@
-// Firebase configuration for Zoom
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
 import {
-  getAuth
+    getAuth
 } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
 import {
-  getFirestore
+    getFirestore
 } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvu3_4yEA03T9XD6MpJIdcQUplp2B2SCc",
-  authDomain: "zoom-5ff91.firebaseapp.com",
-  projectId: "zoom-5ff91",
-  storageBucket: "zoom-5ff91.firebasestorage.app",
-  messagingSenderId: "811034741528",
-  appId: "1:811034741528:web:88fdaa6ca16ac5bc512c6d"
+    apiKey: "AIzaSyCvu3_4yEA03T9XD6MpJIdcQUplp2B2SCc",
+    authDomain: "zoom-5ff91.firebaseapp.com",
+    projectId: "zoom-5ff91",
+    storageBucket: "zoom-5ff91.firebasestorage.app",
+    messagingSenderId: "811034741528",
+    appId: "1:811034741528:web:88fdaa6ca16ac5bc512c6d"
 };
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
+
+const auth =
+    getAuth(app);
+
+const db =
+    getFirestore(app);
 
 
-// Firebase Authentication
-const auth = getAuth(app);
-
-
-// Firestore Database
-const db = getFirestore(app);
-
-
-// Make them available to other files
 export {
-  app,
-  auth,
-  db
+    app,
+    auth,
+    db
 };
